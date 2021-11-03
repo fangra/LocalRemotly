282852d4cf885238e76565d1c6eec684c83ab0cb
Meri fang <raiponce.wa@gmail.com>
2021-11-03 10:48:16 +0100

    cmt

--- .github/workflows/prepareOrg.bat
+++ .github/workflows/prepareOrg.bat
@@ -14,9 +14,15 @@ ECHO End  Do publish Feature1
 ECHO Authorize DEVHUB
 call sfdx force:auth:jwt:grant --clientid "3MVG9t0sl2P.pBypl07KvI.FUHkU1Hw6_7JdMUcR4O4vtd51XoD01z0JyibB.CUjGip1YlyEyHg.MK7GSgUNq" --jwtkeyfile C:\Users\mfangra\KeyOSSL\server3.key --username "meri.wa@gmail.com" -a hug-org -d
 ECHO Scratch Org
-call sfdx force:org:create  --setdefaultusername -f config/project-scratch-def.json -a my-scratch --setalias sorg
+rem call sfdx force:org:create  --setdefaultusername -f config/project-scratch-def.json -a my-scratch --setalias sorg
 ECHO Password generation 
-call sfdx force:user:password:generate --targetusername sorg
+rem call sfdx force:user:password:generate --targetusername sorg
 ECHO Scratch Org Pushing
 call sfdx force:source:push -u sorg
+ECHO Export Data from DEVHUB
+call sfdx force:data:tree:export -q "SELECT Id,Name,(Select Subject from Cases) FROM Account limit 3" -d ./data -p -u hug-org
+ECHO Import Data to sorg
+call sfdx force:data:tree:import -p data/Account-Case-plan.json -u sorg
+ECHO Dispaly Password
+call sfdx force:user:display -u sorg
 PAUSE
