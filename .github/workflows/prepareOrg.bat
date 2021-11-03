a6fc0781e2c1452edba93a018e3ed93e74b457aa
Meri fang <raiponce.wa@gmail.com>
2021-11-03 09:55:33 +0100

    cmt

--- .github/workflows/prepareOrg.bat
+++ .github/workflows/prepareOrg.bat
@@ -1,7 +1,21 @@
 @ECHO OFF
 ECHO Congratulations! Your first steps to prepare your orgs
-ECHO git checkout master
-git checkout master
-ECHO git push
+ECHO Do Checkout
+git checkout DevHub
+ECHO Do current branch
+git branch --show-current
+ECHO Do Feature creation
+git checkout Feature1
+ECHO Feature checking
+git branch --show-current
+ECHO Do publish Feature1
 git push --set-upstream origin Feature1
+ECHO End  Do publish Feature1
+ECHO Authorize DEVHUB
+sfdx force:auth:jwt:grant --clientid "3MVG9t0sl2P.pBypl07KvI.FUHkU1Hw6_7JdMUcR4O4vtd51XoD01z0JyibB.CUjGip1YlyEyHg.MK7GSgUNq" --jwtkeyfile C:\Users\mfangra\KeyOSSL\server3.key --username meri.wa@gmail.com -a hug-org -d
+ECHO Scratch Org
+rem ECHO Password generation 
+rem call sfdx force:user:password:generate --targetusername test-jxl0utitxcyh@example.com
+ECHO Scratch Org Pushing
+call sfdx force:source:push -u test-jxl0utitxcyh@example.com
 PAUSE
