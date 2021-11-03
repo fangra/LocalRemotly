6a91dbb5a9431bb1e9da7ad45bb9244640d403c3
Meri fang <raiponce.wa@gmail.com>
2021-11-03 10:08:02 +0100

    cmt

--- .github/workflows/prepareOrg.bat
+++ .github/workflows/prepareOrg.bat
@@ -12,10 +12,11 @@ ECHO Do publish Feature1
 git push --set-upstream origin Feature1
 ECHO End  Do publish Feature1
 ECHO Authorize DEVHUB
-sfdx force:auth:jwt:grant --clientid "3MVG9t0sl2P.pBypl07KvI.FUHkU1Hw6_7JdMUcR4O4vtd51XoD01z0JyibB.CUjGip1YlyEyHg.MK7GSgUNq" --jwtkeyfile C:\Users\mfangra\KeyOSSL\server3.key --username meri.wa@gmail.com -a hug-org -d
+sfdx force:auth:jwt:grant --clientid "3MVG9t0sl2P.pBypl07KvI.FUHkU1Hw6_7JdMUcR4O4vtd51XoD01z0JyibB.CUjGip1YlyEyHg.MK7GSgUNq" --jwtkeyfile C:\Users\mfangra\KeyOSSL\server3.key --username "meri.wa@gmail.com" -a hug-org -d
 ECHO Scratch Org
-rem ECHO Password generation 
-rem call sfdx force:user:password:generate --targetusername test-jxl0utitxcyh@example.com
+sfdx force:org:create  --setdefaultusername -f config/project-scratch-def.json -a my-scratch --setalias sorg
+ECHO Password generation 
+sfdx force:user:password:generate --targetusername sorg
 ECHO Scratch Org Pushing
-call sfdx force:source:push -u test-jxl0utitxcyh@example.com
+sfdx force:source:push -u sorg
 PAUSE
