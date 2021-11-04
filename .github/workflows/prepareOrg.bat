3a0dddb990b75f61fe0e6da746b122dd12919c38
Meri fang <raiponce.wa@gmail.com>
2021-11-04 22:01:31 +0100

    commit

--- .github/workflows/prepareOrg.bat
+++ .github/workflows/prepareOrg.bat
@@ -2,7 +2,8 @@
 
 set /p DevHub=Enter your DEV Branch Name : 
 set /p Feature=Enter your Feature Branch Name :
-set /p sqllist=Enter your SQL Querys Name :
+set /p WData=Do you want to populate your scratch org ? [y/n]:
+if %WData%==y ( set /p sqllist=Enter your SQL Querys Name :)
 
 ECHO #####################################
 ECHO Start preparing your orgs and branches..
@@ -31,27 +32,30 @@ call sfdx force:user:password:generate --targetusername sorg
 ECHO Push into Scratch Org :
 call sfdx force:source:push -f -u sorg
 
-ECHO Export Data from DEVHUB 
+IF %WData%==y (
+    ECHO Export Data from DEVHUB 
 
-for %%a in %sqllist% do (
-    echo Start Export using Query : %%a 
-    echo ##################
-    call sfdx force:data:tree:export -q %%a -d ./data -p -u hug-org 
-    echo End Export using Query
-    echo ##################
-)
+    for %%a in %sqllist% do (
+        echo Start Export using Query : %%a 
+        echo ##################
+        call sfdx force:data:tree:export -q %%a -d ./data -p -u hug-org 
+        echo End Export using Query
+        echo ##################
+    )
 
-ECHO Import Data to sorg
+    ECHO Import Data to sorg
 
-set /p Files=Enter your plan files to Import, exemple Account-Case-plan :
-for %%a in %Files% do (
-    echo %%a
-    echo Start Import of %%a Data
-    echo ##################
-    call sfdx force:data:tree:import -p data/%%a.json -u sorg
-    echo End Import. 
-    ECHO ##################
-)
+    set /p Files=Enter your plan files to Import, exemple Account-Case-plan :
+    for %%a in %Files% do (
+        echo %%a
+        echo Start Import of %%a Data
+        echo ##################
+        call sfdx force:data:tree:import -p data/%%a.json -u sorg
+        echo End Import. 
+        ECHO ##################
+    )
+) 
+
 
 ECHO Dispaly Scratch Org Information :
 call sfdx force:user:display -u sorg
