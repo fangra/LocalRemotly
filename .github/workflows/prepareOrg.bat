3ac702249469e26344b923026d3a1e431584903c
Meri fang <raiponce.wa@gmail.com>
2021-11-04 13:13:36 +0100

    Commit

--- .github/workflows/prepareOrg.bat
+++ .github/workflows/prepareOrg.bat
@@ -1,20 +1,27 @@
 @ECHO OFF
-set list=(Account, Case)
+rem set /p list=Enter your objects exemple (Account,Case): 
+set /p DevHub=Enter your DEV Branch Name : 
+set /p Feature=Enter your Feature Branch Name :
+set /p sqllist=Enter your SQL Querys Name :
+set Cli="3MVG9t0sl2P.pBypl07KvI.FUHkU1Hw6_7JdMUcR4O4vtd51XoD01z0JyibB.CUjGip1YlyEyHg.MK7GSgUNq"
+set pathkey=C:\Users\mfangra\KeyOSSL\server3.key
+set username="meri.wa@gmail.com"
+rem set list=(Account,Case)
 ECHO #####################################
 ECHO Congratulations! Your first steps to prepare your orgs
 ECHO Do Checkout
-git checkout DevHub
+git checkout %DevHub%
 ECHO Do current branch
 git branch --show-current
 ECHO Do Feature creation
-git checkout -b Feature1 DEVHUB
+git checkout -b %Feature% %DevHub%
 ECHO Feature checking
 git branch --show-current
-ECHO Do publish Feature1
-git push --set-upstream origin Feature1
-ECHO End  Do publish Feature1
+ECHO Do publish %Feature%
+git push --set-upstream origin %Feature%
+ECHO End  Do publish %Feature%
 ECHO Authorize DEVHUB
-call sfdx force:auth:jwt:grant --clientid "3MVG9t0sl2P.pBypl07KvI.FUHkU1Hw6_7JdMUcR4O4vtd51XoD01z0JyibB.CUjGip1YlyEyHg.MK7GSgUNq" --jwtkeyfile C:\Users\mfangra\KeyOSSL\server3.key --username "meri.wa@gmail.com" -a hug-org -d
+call sfdx force:auth:jwt:grant --clientid %Cli% --jwtkeyfile %pathkey%  --username %username% -a hug-org -d
 ECHO Scratch Org
 rem call sfdx force:org:create  --setdefaultusername -f config/project-scratch-def.json -a my-scratch --setalias sorg
 ECHO Password generation 
@@ -24,25 +31,34 @@ call sfdx force:source:retrieve -x C:\Users\mfangra\Desktop\Local\LocalRemotly\.
 ECHO Scratch Org Pushing
 call sfdx force:source:push -f -u sorg
 ECHO Export Data from DEVHUB
-rem call sfdx force:data:tree:export -q "SELECT FIELDS(ALL) FROM Account limit 3" -d ./data.csv -p -u hug-org 
 set n=0
-for %%a in %list% do (
+rem for %%a in %list% do (
+for %%a in %sqllist% do (
 echo Start Query of SObject : %%a 
 echo ##################
-if %%a == Account call sfdx force:data:soql:query -q "SELECT  Name,FIELDS(CUSTOM) FROM %%a LIMIT 10" -u=hug-org -r=csv > %%a.csv
-if %%a == Case call sfdx force:data:soql:query -q "SELECT  Subject,SLAViolation__c,Product__c,PotentialLiability__c FROM %%a LIMIT 10" -u=hug-org -r=csv > %%a.csv
+rem if %%a == Account call sfdx force:data:soql:query -q "SELECT  Name,FIELDS(CUSTOM) FROM %%a LIMIT 10" -u=hug-org -r=csv > %%a.csv
+rem if %%a == Case call sfdx force:data:soql:query -q "SELECT  Subject,Account.SF_Account_Number__c,SLAViolation__c,Product__c,PotentialLiability__c FROM %%a LIMIT 10" -u=hug-org -r=csv > %%a.csv
+rem if %%a == Account call sfdx force:data:tree:export -q "SELECT Id,Name,FIELDS(CUSTOM) FROM %%a LIMIT 10" -d ./data -p -u hug-org 
+rem if %%a == Case call sfdx force:data:tree:export -q "SELECT Id,Subject,FIELDS(CUSTOM) FROM %%a LIMIT 10" -d ./data -p -u hug-org 
+rem if %%a == Case call sfdx force:data:tree:export -q "SELECT  Id,Name, (SELECT Id,Subject FROM Cases) FROM Account LIMIT 10" -d ./data -p -u hug-org 
+call sfdx force:data:tree:export -q %%a -d ./data -p -u hug-org 
 echo End Query 
 echo ##################
 )
 ECHO Import Data to sorg
 rem call sfdx force:data:tree:import -p data/Account-Case-plan.json -u sorg
+set /p Files=Enter your Files to Import :
 set n=0
-for %%a in %list% do (
+rem for %%a in %list% do (
+for %%a in %Files% do (
 echo %%a
 echo Start Upsert of %%a  's Data
 echo ##################
-echo sfdx force:data:bulk:upsert -s %%a -f %%a.csv -i Id -w 2 -u sorg
-call sfdx force:data:bulk:upsert -s %%a -f %%a.csv -i Id -w 2 -u sorg
+rem echo sfdx force:data:bulk:upsert -s %%a -f %%a.csv -i Id -w 2 -u sorg
+rem call sfdx force:data:bulk:upsert -s %%a -f %%a.csv -i Id -w 2 -u sorg
+rem sfdx force:data:tree:import -p data/%%a-plan.json -u sorg
+rem if %%a == Case  sfdx force:data:tree:import -p data/%%a.json -u sorg
+sfdx force:data:tree:import -p data/%%a.json -u sorg
 echo End Upsert 
 echo ##################
 )
