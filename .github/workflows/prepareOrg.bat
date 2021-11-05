05e99e8692563f0c80596ee7b3ef4761333a5d90
Meri fang <raiponce.wa@gmail.com>
2021-11-05 09:27:34 +0100

    commit

--- .github/workflows/prepareOrg.bat
+++ .github/workflows/prepareOrg.bat
@@ -1,9 +1,12 @@
 @ECHO OFF
-
+setlocal EnableExtensions EnableDelayedExpansion
 set /p DevHub=Enter your DEV Branch Name : 
 set /p Feature=Enter your Feature Branch Name :
+set /p sorg=Enter your Scratch Organisation Alias :
 set /p WData=Do you want to populate your scratch org ? [y/n]:
-if %WData%==y ( set /p sqllist=Enter your SQL Querys Name :)
+
+IF %WData%==y ( set /p sqllist=Enter your SQL Querys Name :)
+
 
 ECHO #####################################
 ECHO Start preparing your orgs and branches..
@@ -23,17 +26,21 @@ git branch --show-current
 ECHO Publish %Feature% :
 git push --set-upstream origin %Feature%
 
-ECHO Create Scratch Org :
-rem call sfdx force:org:create  --setdefaultusername -f config/project-scratch-def.json -a my-scratch --setalias sorg
+ECHO Create %sorg% Scratch Org :
+rem call sfdx force:org:create  --setdefaultusername -f config/project-scratch-def.json -a my-scratch --setalias %sorg%
 
-ECHO Generate Scratch Org Password :
-call sfdx force:user:password:generate --targetusername sorg
+ECHO Generate %sorg% Password :
+call sfdx force:user:password:generate --targetusername %sorg%
 
-ECHO Push into Scratch Org :
-call sfdx force:source:push -f -u sorg
+
+ECHO Push into %sorg% Scratch Org :
+call sfdx force:source:push -u sorg
+
+ECHO %WData%
 
 IF %WData%==y (
-    ECHO Export Data from DEVHUB 
+
+    ECHO Export Data from DEVHUB :
 
     for %%a in %sqllist% do (
         echo Start Export using Query : %%a 
@@ -43,24 +50,24 @@ IF %WData%==y (
         echo ##################
     )
 
-    ECHO Import Data to sorg
+    ECHO Import Data to %sorg% :
 
     set /p Files=Enter your plan files to Import, exemple Account-Case-plan :
     for %%a in %Files% do (
         echo %%a
         echo Start Import of %%a Data
         echo ##################
-        call sfdx force:data:tree:import -p data/%%a.json -u sorg
+        call sfdx force:data:tree:import -p data/%%a.json -u %sorg%
         echo End Import. 
         ECHO ##################
     )
-) 
+)
 
 
-ECHO Dispaly Scratch Org Information :
-call sfdx force:user:display -u sorg
+ECHO Dispaly %sorg% Scratch Org Information :
+call sfdx force:user:display -u %sorg%
 
 ECHO End of Preparation.
 ECHO #####################################
 
-EXIT
+PAUSE
